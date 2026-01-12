38d8383f339efeace434312e420a95b9e23d2b8e
andrea.sabini <andrea.sabini@spindox.it>
2026-01-12 12:58:07 +0100

    fix error

--- magazzino/src/main/java/it/spindox/stagelab/magazzino/MagazzinoApplication.java
+++ magazzino/src/main/java/it/spindox/stagelab/magazzino/MagazzinoApplication.java
@@ -2,12 +2,22 @@ package it.spindox.stagelab.magazzino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class MagazzinoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagazzinoApplication.class, args);
 	}
-
 }
+
