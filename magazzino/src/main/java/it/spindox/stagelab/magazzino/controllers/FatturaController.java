b4d2879686016ba6de4ce24b5b7e16e0d40e7664
andrea.sabini <andrea.sabini@spindox.it>
2026-01-16 16:37:25 +0100

    Exception Integration

--- magazzino/src/main/java/it/spindox/stagelab/magazzino/controllers/FatturaController.java
+++ magazzino/src/main/java/it/spindox/stagelab/magazzino/controllers/FatturaController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RequestMapping("/api/fatture")
 public class FatturaController {
 
+    
     private final FatturaService service;
 
     public FatturaController(FatturaService service) {
