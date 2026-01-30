9f53b49068446236dac25d92a420274e56917506
elia.sollazzo <elia.sollazzo@spindox.it>
2026-01-30 16:42:17 +0100

    codice funzionante con tutto , minimal stow

--- magazzino/src/main/java/it/spindox/stagelab/magazzino/Sjobs/InventoryScheduler.java
+++ magazzino/src/main/java/it/spindox/stagelab/magazzino/Sjobs/InventoryScheduler.java
@@ -1,12 +1,18 @@
 package it.spindox.stagelab.magazzino.Sjobs;
+import java.util.logging.Logger;
 import it.spindox.stagelab.magazzino.services.MagazzinoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+
+@Slf4j
 @Component
 @EnableScheduling
 public class InventoryScheduler {
+
+    private static final Logger logger = Logger.getLogger(InventoryScheduler.class.getName());
 
     private final MagazzinoService magazzinoService;
 
@@ -16,6 +22,10 @@ public class InventoryScheduler {
 
     @Scheduled(fixedRateString = "${inventory.check.rate}")
     public void runCheck() {
+        logger.info(" Avvio job: controllo livelli di stock");
+
         magazzinoService.checkStockLevels();
+
+        logger.info(" Fine job: controllo completato");
     }
 }
